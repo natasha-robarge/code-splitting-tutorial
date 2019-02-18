8776d62dc4275e2b882e7baae7594d96332ac96f
natasha-robarge <robarge.natasha7@gmail.com>
2019-02-18 09:26:11 -0600

    add routing logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import { Page1, Page2, Page3 } from './components';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route})
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.state.route === 'page1')
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    else if (this.state.route === 'page2')
+      return <Page2 onRouteChange={this.onRouteChange} />;
+    else 
+      return <Page3 onRouteChange={this.onRouteChange} />;
+    
   }
 }
 
