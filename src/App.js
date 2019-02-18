6b1cace0503a6d4b5272dad6d81be1a265809ae8
natasha-robarge <robarge.natasha7@gmail.com>
2019-02-18 13:41:45 -0600

    add code split logic to handle the asynchrounous component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Page1 from './components/Page1';
+import AsyncComponent from './components/AsyncComponent';
 import './App.css';
 
 class App extends Component {
@@ -9,30 +10,33 @@ class App extends Component {
 
     this.state = {
       route: 'page1',
-      component: ''
+      component: null
     }
   }
 
   onRouteChange = (route) => {
     //No code splitting
-    // this.setState({route})
+    this.setState({route})
 
     //With code splitting
-    if (route === 'page1')
-      this.setState({ route });
-    else if (route === 'page2')
-      import('./components/Page2').then((Page2) => this.setState({ route: route, component: Page2.default }));
-    else
-      import('./components/Page3').then((Page3) => this.setState({ route: route, component: Page3.default }));
-
+    // if (route === 'page1')
+    //   this.setState({ route });
+    // else if (route === 'page2')
+    //   import('./components/Page2').then((Page2) => this.setState({ route: route, component: Page2.default }));
+    // else
+    //   import('./components/Page3').then((Page3) => this.setState({ route: route, component: Page3.default }));
   }
 
   render() {
-    if (this.state.route === 'page1')
-      return <Page1 onRouteChange={this.onRouteChange} />;
-    else 
-      return <this.state.component onRouteChange={this.onRouteChange} />;
-    
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    } else {
+      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />
+    }
   }
 }
 
